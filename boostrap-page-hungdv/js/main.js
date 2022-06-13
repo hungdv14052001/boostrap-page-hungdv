c6d7ea0b11df46af6895fee9c1914dc4854dcb90
hungdv14052001 <hung14052001xyz@gmail.com>
2022-06-13 17:43:09 +0700

    1st commit (Đã Responsive)

---
+++ boostrap-page-hungdv/js/main.js
@@ -0,0 +1,8 @@
+/*Open and Close Search*/
+const searchModal= document.querySelector('.search-contairner');
+function openSearchModal(){
+    searchModal.classList.add('open-Search');
+}
+function closeSearchModal(){
+    searchModal.classList.remove('open-Search');
+}
